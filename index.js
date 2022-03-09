9ab61edaac61d890da49ec59e384b824afd2ee3a
BellaPulatova <bellakhan2327@outlook.com>
2022-03-09 23:44:44 +0900

    js

---
+++ index.js
@@ -0,0 +1,11 @@
+var name ='Bernura'
+var age = 26
+var status = 'student'
+var object = {
+    name: 'bernura',
+    age: 26,
+    status: 'developer'
+}
+
+console.log('mening ismim',name, 'mening yoshim', age)
+console.log(object.name)
