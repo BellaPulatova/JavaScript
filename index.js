aa0385151399588e39bd61eee16f324a7180fb3e
BellaPulatova <bellakhan2327@outlook.com>
2022-03-11 16:36:05 +0900

    js

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 
-// function test(){
-//     var name = 'wbba'
-//     return name + ' surname'
-// }
-// console.log(test())
+function test(){
+    var name = 'wbba'
+    return name + ' surname'
+}
+console.log(test())
 
-// var wbba = function(){
-//     console.log('wbba')
-// }
-// wbba();
+var wbba = function(){
+    console.log('wbba')
+}
+wbba();
 function sum ( a, b){
     console.log(a+b)
 }
 sum(3,4)
+
+var time = 'xufton'
+if (time === 'kun') {
+    console.log('light off')
+}
+else if (time==='tun') {
+    console.log('light on')
+}
+else if (time==='shom'){
+    console.log('ihtiyoriy')
+}
+else{
+    console.log('hech nima')
+};
+
+function sum(a,b){
+    if(a>b){
+        console.log(a-b)
+    }
+    else if (a<b){
+        console.log(b-a)
+    }
+    else{
+        console.log(a-b)
+    }
+}
+sum (4,5)
+sum (5,4)
+
